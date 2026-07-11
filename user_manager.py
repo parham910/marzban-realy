2ce758c271a7170a8f27816f322a44e7d55a2269
parham910 <bhibijgr@atomicmail.io>
2026-07-11 16:06:08 +0330

    Update user_manager.py

--- user_manager.py
+++ user_manager.py
@@ -2,8 +2,11 @@ from models import User, engine
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime, timedelta
 import uuid
+import json
+import os
 
 Session = sessionmaker(bind=engine)
+XRAY_CONFIG = "/app/xray_config.json"
 
 class UserManager:
     @staticmethod
@@ -19,12 +22,34 @@ class UserManager:
         )
         session.add(user)
         session.commit()
+        UserManager.add_user_to_xray(user)
         return user
+
+    @staticmethod
+    def add_user_to_xray(user):
+        if not os.path.exists(XRAY_CONFIG):
+            return False
+        with open(XRAY_CONFIG, 'r') as f:
+            config = json.load(f)
+        new_client = {
+            "id": user.uuid,
+            "flow": "xtls-rprx-vision",
+            "email": user.username
+        }
+        config["inbounds"][0]["settings"]["clients"].append(new_client)
+        with open(XRAY_CONFIG, 'w') as f:
+            json.dump(config, f, indent=2)
+        return True
 
     @staticmethod
     def get_user_by_username(username):
         session = Session()
         return session.query(User).filter_by(username=username).first()
+
+    @staticmethod
+    def get_user_by_id(user_id):
+        session = Session()
+        return session.query(User).filter_by(id=user_id).first()
 
     @staticmethod
     def get_all_users():
